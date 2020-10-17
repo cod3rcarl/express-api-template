705273c280490f4f44883ac5abee59de417ebddd
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2020-10-17 17:45:43 +0100

    01 install

---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+router.get('/', function(req, res, next) {
+  return res.status(200).json({ message: 'Welcome to Express API template' });
+});
+module.exports = router;
